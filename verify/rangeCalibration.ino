139a3c35e9f40ddf58776bfb0cbf74b11011753b
EnigMoiD <evan.dorsky@icloud.com>
2013-09-13 12:01:58 -0400

    Button press outputs short burst of serial data

---
+++ verify/rangeCalibration.ino
@@ -0,0 +1,52 @@
+const int analogInPin = A0;
+const int analogOutPin = 9;
+
+long currentMillis = 0;
+
+int sensorValue = 0;
+int outputValue = 0;
+
+// Button stuff
+int lastButtonState = 0;
+int buttonState = 0;
+int buttonPin = 8;
+long buttonTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT); 
+}
+
+void outputData(int count) {
+  Serial.println("Data points");
+  Serial.println("--------------------");
+  for (int i = 0; i < count; i ++) {
+    sensorValue = analogRead(analogInPin);
+
+    Serial.println(sensorValue);
+
+    delay(2);
+  }
+  Serial.println("====================");
+}
+
+void loop() { 
+  currentMillis = millis();
+
+  lastButtonState = buttonState;
+  buttonState = digitalRead(buttonPin);
+
+  if (lastButtonState != buttonState) {
+    if (lastButtonState == LOW && buttonState == HIGH) {
+      buttonTime = millis();
+    }
+    else if (lastButtonState == HIGH && buttonState == LOW) {
+      unsigned long delta = currentMillis - buttonTime;
+
+      if (delta > 50) {
+        outputData(10);
+      }
+    }
+  }
+}
